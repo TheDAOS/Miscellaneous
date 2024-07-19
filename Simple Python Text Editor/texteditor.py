7de11ef9b3455ccd6b2915a99e36e9ed5a48a461
Sanju Santhosh <68913047+TheDAOS@users.noreply.github.com>
2024-07-19 07:51:25 +0000

    Adding Font Changer

--- Simple Python Text Editor/texteditor.py
+++ Simple Python Text Editor/texteditor.py
@@ -6,6 +6,7 @@ root=Tk()
 text=Text(root)
 text.grid()
 
+# Save Button
 def saveas():
     global text
     t = text.get("1.0", "end-1c")
@@ -16,5 +17,24 @@ def saveas():
     
 button=Button(root, text="Save", command=saveas)
 button.grid()
+
+# Font Changer
+def FontHelvetica():
+    global text
+    text.config(font="Helvetica")
+    
+def FontCourier():
+    global text
+    text.config(font="Courier")
+    
+font = Menubutton(root, text="Font")
+font.grid()
+font.menu=Menu(font, tearoff=0)
+font["menu"]=font.menu
+helvetica=IntVar()
+courier=IntVar()
+
+font.menu.add_checkbutton(label="Courier", variable=courier, command=FontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=helvetica, command=FontHelvetica)
 
 root.mainloop()
