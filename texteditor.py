ec0ddbd61238977f273d2045cfba8f7839e4d5cd
Sanju Santhosh <68913047+TheDAOS@users.noreply.github.com>
2024-07-19 06:40:06 +0000

    0

--- texteditor.py
+++ texteditor.py
@@ -1,7 +1,20 @@
 from tkinter import *
+from tkinter import filedialog
+
 root=Tk()
 
 text=Text(root)
 text.grid()
+
+def saveas():
+    global text
+    t = text.get("1.0", "end-1c")
+    savelocation=filedialog.asksaveasfilename()
+    file1=open(savelocation, "W+")
+    file1.write(t)
+    file1.close()
+    
+button=Button(root, text="Save", command=saveas)
+button.grid()
 
 root.mainloop()
