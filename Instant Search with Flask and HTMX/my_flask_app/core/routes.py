7fb38e9f7a8ebcfbe7160288f162528bb3a05963
theDAOS <sanjusanthosh46@gmail.com>
2024-09-08 15:39:45 +0530

    Create the Search Route

--- Instant Search with Flask and HTMX/my_flask_app/core/routes.py
+++ Instant Search with Flask and HTMX/my_flask_app/core/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from core import app
 from core.models import Book
 
@@ -7,3 +7,12 @@ def index():
     # Fetch the first 20 books to display by default
     books = Book.query.limit(20).all()
     return render_template("index.html", books=books)
+
+@app.route('/search')
+def search():
+    query = request.args.get("query")
+    if query:
+        results = Book.query.filter(Book.title.ilike(f"%{query}%") | Book.author.ilike(f"%{query}%")).limit(10).all()
+    else:
+        results = Book.query.limit(20).all()
+    return render_template("search_results.html", results=results)
