bcc9bbab6be671c5eb903a8aee04d789ea770859
theDAOS <sanjusanthosh46@gmail.com>
2024-09-08 14:08:29 +0530

    flask urls

--- Flask/learning flask/hello.py
+++ Flask/learning flask/hello.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
