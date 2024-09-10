909fb500b70772c8f28cbcfa3b46bb625d1cf959
theDAOS <sanjusanthosh46@gmail.com>
2024-09-10 10:36:33 +0530

    script.js

--- Simple Bitcoin-to-USD Calculator/script.js
+++ Simple Bitcoin-to-USD Calculator/script.js
@@ -1,21 +1,21 @@
 const API_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    // Run an asynchronous function once the DOM content has loaded
-    let bitcoinPrice; // Initialize the variable
+    let bitcoinPrice = localStorage.getItem("lastBitcoinPrice");
+    // Retrieve the last stored Bitcoin price from localStorage, if it exists
+    // Note: It should be null the first time you try running the page
   
     try {
       const response = await fetch(API_URL);
-      // Await a response from the HTTP request sent to the API URL
-  
       const data = await response.json();
-      // Await the json content of the response
-  
       bitcoinPrice = data.bpi.USD.rate_float.toFixed(2);
-      // Reassign the bitcoinPrice variable to the USD value of Bitcoin, rounded to the nearest 2 decimal places.
+      // bitcoinPrice will be re-written
+  
+      localStorage.setItem("lastBitcoinPrice", bitcoinPrice);
+      // Save most recent price to localStorage
     } catch {
-      console.log("error!"); // In case of error
+      console.log("error!");
     }
   
-    console.log(bitcoinPrice); // Log the price in the console
-});
+    console.log(bitcoinPrice);
+  });
