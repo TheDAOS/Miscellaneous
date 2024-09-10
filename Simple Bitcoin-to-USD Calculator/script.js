3c93ebd697ac24f9f702f0f087255befd14af37e
theDAOS <sanjusanthosh46@gmail.com>
2024-09-10 10:37:34 +0530

    script

--- Simple Bitcoin-to-USD Calculator/script.js
+++ Simple Bitcoin-to-USD Calculator/script.js
@@ -1,20 +1,31 @@
 const API_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 document.addEventListener("DOMContentLoaded", async () => {
+    const main = document.getElementById("main");
+    const bitcoinPriceElement = document.getElementById("bitcoinPrice");
+    const bitcoinAmountInput = document.getElementById("bitcoinAmount");
+    const calculateBtn = document.getElementById("calculateBtn");
+    const usdAmountElement = document.getElementById("usdAmount");
+  
     let bitcoinPrice = localStorage.getItem("lastBitcoinPrice");
-    // Retrieve the last stored Bitcoin price from localStorage, if it exists
-    // Note: It should be null the first time you try running the page
   
     try {
       const response = await fetch(API_URL);
       const data = await response.json();
       bitcoinPrice = data.bpi.USD.rate_float.toFixed(2);
-      // bitcoinPrice will be re-written
-  
       localStorage.setItem("lastBitcoinPrice", bitcoinPrice);
-      // Save most recent price to localStorage
+  
+      bitcoinPriceElement.textContent = bitcoinPrice;
+      // Set the text content of bitcoinPriceElement to the current bitcoinPrice
     } catch {
-      console.log("error!");
+      if (bitcoinPrice) {
+        // If there's an existing bitcoinPrice in localStorage...
+        bitcoinPriceElement.textContent = bitcoinPrice;
+        // ...display whatever is saved localStorage
+      } else {
+        main.innerHTML = "<p>Could not fetch Bitcoin price :(</p>";
+        return;
+      }
     }
   
     console.log(bitcoinPrice);
