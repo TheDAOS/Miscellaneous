70d183323c076b6ad68e674ee8ba29630c8014aa
theDAOS <sanjusanthosh46@gmail.com>
2024-09-23 11:49:35 +0530

    darw circle

--- Animated Bubbles/Script.js
+++ Animated Bubbles/Script.js
@@ -15,3 +15,12 @@ const handleDrawCircle = (event) => {
   // Draw a bubble!
   drawCircle(x, y);
 };
+
+const drawCircle = (x, y) => {
+    context.beginPath();
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+  
+    context.strokeStyle = 'white';
+    context.stroke();
+  };
+  
