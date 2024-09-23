da82916996066a1dc1c478cb52f73da4b44f69e9
theDAOS <sanjusanthosh46@gmail.com>
2024-09-23 11:48:40 +0530

    adding x and y

--- Animated Bubbles/Script.js
+++ Animated Bubbles/Script.js
@@ -5,3 +5,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 canvas.addEventListener('click', handleDrawCircle);
+
+//We are adding x and y here because we will need it later.
+let x, y
+const handleDrawCircle = (event) => {
+  x = event.pageX;
+  y = event.pageY;
+
+  // Draw a bubble!
+  drawCircle(x, y);
+};
